285746d982436270d85d475718f27a8afb7424ac
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2024-12-07 15:16:29 +0100

    Create custom images

--- build.sh
+++ build.sh
@@ -4,20 +4,15 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+rpm-ostree install \
+	bat \
+	eza \
+        syncthing \
+        vim \
+        zsh \
+        zsh-autosuggestions \
+        zsh-syntax-highlighting \
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
+sed -i 's|^SHELL=.*|SHELL=/usr/bin/zsh|' /etc/default/useradd
 
 systemctl enable podman.socket
